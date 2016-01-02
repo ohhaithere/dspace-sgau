ba4890ad9362c2e5ff0935524de62d0468f3219e
Igor <frostmasterwork@gmail.com>
2016-01-02 09:05:39 -0800

    Import Start

---
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/ImportServlet.java
@@ -0,0 +1,31 @@
+package org.dspace.app.webui.servlet;
+
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.core.Context;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by root on 1/1/16.
+ */
+public class ImportServlet {
+
+    protected void doDSGet(Context context, HttpServletRequest request,
+                           HttpServletResponse response) throws ServletException, IOException,
+            SQLException, AuthorizeException {
+
+    }
+
+    protected void doDSPost(Context context, HttpServletRequest request,
+                            HttpServletResponse response) throws ServletException, IOException,
+            SQLException, AuthorizeException{
+
+
+    }
+
+
+}
